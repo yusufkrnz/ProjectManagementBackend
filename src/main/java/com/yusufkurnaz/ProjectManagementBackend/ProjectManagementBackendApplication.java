d45223c231e049c433c01739af18a53db664f91c
yusufkurnaz <wayneysf@gmail.com>
2025-09-09 17:32:41 +0300

    Start

---
+++ src/main/java/com/yusufkurnaz/ProjectManagementBackend/ProjectManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yusufkurnaz.ProjectManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusufkurnaz/ProjectManagementBackend/ProjectManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufkurnaz.ProjectManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
